259b00ccf18caa7d38d6ca42029041c4477c6f93
Kirishima21 <mastodon@mail.kirishima.cloud>
2020-04-09 06:45:43 +0900

    fix content_security_policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -2,9 +2,9 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# if Rails.env.production?
-#  assets_host = Rails.configuration.action_controller.asset_host || "https://#{ENV['WEB_DOMAIN'] || ENV['LOCAL_DOMAIN']}"
-#  data_hosts = [assets_host]
+if Rails.env.production?
+  assets_host = Rails.configuration.action_controller.asset_host || "https://#{ENV['WEB_DOMAIN'] || ENV['LOCAL_DOMAIN']}"
+  data_hosts = [assets_host]
 
   if ENV['S3_ENABLED'] == 'true'
     attachments_host = "https://#{ENV['S3_ALIAS_HOST'] || ENV['S3_CLOUDFRONT_HOST'] || ENV['S3_HOSTNAME'] || "s3-#{ENV['S3_REGION'] || 'us-east-1'}.amazonaws.com"}"
