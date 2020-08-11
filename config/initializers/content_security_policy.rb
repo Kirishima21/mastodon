0719fcfa10724610fd68c6e00b126905e2214f72
kirishima21 <mastodon@mail.kirishima.cloud>
2020-08-11 18:18:03 +0900

    Revert "fix csp"
    This reverts commit 1655cc081cc7f9247993d7cd8727dff5d912b845.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,8 +5,6 @@
 if Rails.env.production?
   assets_host = Rails.configuration.action_controller.asset_host || "https://#{ENV['WEB_DOMAIN'] || ENV['LOCAL_DOMAIN']}"
   data_hosts = [assets_host]
-  cloudflarecdn = 'https://cdnjs.cloudflare.com'
-  mathjax = 'https://cdn.mathjax.org'
 
   if ENV['S3_ENABLED'] == 'true'
     attachments_host = "https://#{ENV['S3_ALIAS_HOST'] || ENV['S3_CLOUDFRONT_HOST'] || ENV['S3_HOSTNAME'] || "s3-#{ENV['S3_REGION'] || 'us-east-1'}.amazonaws.com"}"
