d4e284481837c0c467086a3d2b234f9697c0ed4d
霧島ひなた <mastodon@mail.kirishima.cloud>
2021-10-08 20:25:54 +0900

    change CustomEmoji size

--- app/models/custom_emoji.rb
+++ app/models/custom_emoji.rb
@@ -21,7 +21,7 @@
 #
 
 class CustomEmoji < ApplicationRecord
-  LOCAL_LIMIT = (ENV['MAX_EMOJI_SIZE'] || 50.kilobytes).to_i
+  LOCAL_LIMIT = (ENV['MAX_EMOJI_SIZE'] || 150.kilobytes).to_i
   LIMIT       = [LOCAL_LIMIT, (ENV['MAX_REMOTE_EMOJI_SIZE'] || 200.kilobytes).to_i].max
 
   SHORTCODE_RE_FRAGMENT = '[a-zA-Z0-9_]{2,}'
