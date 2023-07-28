9ce9ce9e31b00522f3a2157811dc36921fa7b6b8
Kirishima21 <hello@krsm.me>
2023-07-29 06:04:42 +0900

    migrateをすこし変更した

--- db/migrate/20180820232245_add_foreign_key_indices.rb
+++ db/migrate/20180820232245_add_foreign_key_indices.rb
@@ -4,13 +4,13 @@ class AddForeignKeyIndices < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
   def change
-    add_index :follows, :target_account_id, algorithm: :concurrently
-    add_index :blocks, :target_account_id, algorithm: :concurrently
-    add_index :mutes, :target_account_id, algorithm: :concurrently
-    add_index :notifications, :from_account_id, algorithm: :concurrently
-    add_index :accounts, :moved_to_account_id, algorithm: :concurrently
-    add_index :statuses, :in_reply_to_account_id, algorithm: :concurrently
-    add_index :session_activations, :access_token_id, algorithm: :concurrently
-    add_index :oauth_access_grants, :resource_owner_id, algorithm: :concurrently
+    add_index :follows, :target_account_id, algorithm: :concurrently unless index_exists?(:follows, :target_account_id)
+    add_index :blocks, :target_account_id, algorithm: :concurrently unless index_exists?(:blocks, :target_account_id)
+    add_index :mutes, :target_account_id, algorithm: :concurrently unless index_exists?(:mutes, :target_account_id)
+    add_index :notifications, :from_account_id, algorithm: :concurrently unless index_exists?(:notifications, :from_account_id)
+    add_index :accounts, :moved_to_account_id, algorithm: :concurrently unless index_exists?(:accounts, :moved_to_account_id)
+    add_index :statuses, :in_reply_to_account_id, algorithm: :concurrently unless index_exists?(:statuses, :in_reply_to_account_id)
+    add_index :session_activations, :access_token_id, algorithm: :concurrently unless index_exists?(:session_activations, :access_token_id)
+    add_index :oauth_access_grants, :resource_owner_id, algorithm: :concurrently unless index_exists?(:oauth_access_grants, :resource_owner_id)
   end
 end
